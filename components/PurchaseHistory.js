0ad2c68e55ef9f309552d4e1e4d0fcfacb8c22a6
Nishita Shah <shahnishita76@gmail.com>
2025-03-04 17:08:03 +0530

    Updated PurchaseHistory component

--- components/PurchaseHistory.js
+++ components/PurchaseHistory.js
@@ -3,8 +3,7 @@ import React, { useEffect, useState } from "react";
 const PurchaseHistory = ({ customerId }) => {
   const [history, setHistory] = useState([]);
 
-  useEffect(() => {
-    if (customerId) {
+  useEffect(() =>if (customerId) {
       fetch(`http://127.0.0.1:5000/purchase-history/${customerId}`)
         .then(response => {
           if (!response.ok) {
